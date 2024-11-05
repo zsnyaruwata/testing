df425f5b6240118779a309e26780f7d392731a29
zsnyaruwata <zsnyaruwata@gmail.com>
2024-11-05 16:02:19 +0200

    Add files via upload

---
+++ send-email.php
@@ -0,0 +1,33 @@
+<?php
+require_once "vendor/autoload.php";
+$mail = new PHPMailer;
+//Enable SMTP debugging.
+$mail->SMTPDebug = 3;                           
+//Set PHPMailer to use SMTP.
+$mail->isSMTP();        
+//Set SMTP host name                      
+$mail->Host = "smtp.gmail.com";
+//Set this to true if SMTP host requires authentication to send email
+$mail->SMTPAuth = true;                      
+//Provide username and password
+$mail->Username = "name@gmail.com";             
+$mail->Password = "super_secret_password";                       
+//If SMTP requires TLS encryption then set it
+$mail->SMTPSecure = "tls";                       
+//Set TCP port to connect to
+$mail->Port = 587;                    
+$mail->From = "name@gmail.com";
+$mail->FromName = "Full Name";
+$mail->addAddress("name@example.com", "Recepient Name");
+$mail->isHTML(true);
+$mail->Subject = "Subject Text";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+if(!$mail->send())
+{
+echo "Mailer Error: " . $mail->ErrorInfo;
+}
+else
+{
+echo "Message has been sent successfully";
+}
